84e93ace7eb057d5527bb97be383bd93fc40a87e
tranvo <tranvo@192.168.0.10>
2019-10-09 01:17:00 -0700

    Second version of AddingMachine

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,30 +1,53 @@
 package cse360assign2;
+//Name: Tran Vo
+//Date: 10/07/2019
+//Class ID: 85141
+//Assignment 2
 
 public class AddingMachine {
-
+	//create variables
 	private int total;
+	private String history;
 	
+	//constructor
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
+	//The method that returns the 
+	//total of the math calculation 
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	//Add method
 	public void add (int value) {
-		
+		total = total + value;
+		history = history + " + " + value;
 	}
 	
+	//Subtract method
 	public void subtract (int value) {
-		
+		total = total - value;
+		history = history + " - " + value;
 	}
-		
-	public String toString () {
-		return "";
-	}
-
-	public void clear() {
 	
+	//toString that returns the whole math calculation 
+	//from the beginning
+	public String toString () {
+		return history;
+	}
+	
+	//clear method to reset 
+	public void clear() {
+		total = 0;
+		history = "0";
 	}
 }
+
+
+
+
+
+
